f11504d8e0f953e3768e6491198b3c9581239220
himanshu <himanshusharma2709@gmail.com>
2026-05-10 11:59:16 +0530

    removed user and auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -25,8 +23,6 @@ import { AuthModule } from './auth/auth.module';
         synchronize: true,
       }),
     }),
-    UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
